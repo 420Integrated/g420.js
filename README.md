8069a6b03d12b0a281e60fbcb053eeef01dd0d7e
abvhiael stuart <abvhiael@420integrated.com>
2020-10-28 18:37:38 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
-# g420.js
+g420.js
+=======
+Start and stop [g420](https://github.com/420integrated/go-420coin) from Node.js.
+
+Usage
+-----
+
+```
+$ npm install g420
+```
+To use g420.js, simply require it:
+```javascript
+var g420 = require("g420");
+```
+
+### Starting and stopping g420
+
+g420's `start` method accepts a configuration object, which uses the same flags as the g420 command line client.  (Here, the flags are organized into an object.)  Flags that are not accompanied by a value on the command line (for example, `--mine`) should be passed in as `{ flag: null }`.
+```javascript
+var options = {
+    networkid: "420",
+    port: 13013,
+    rpcport: 6174,
+    mine: null
+};
+
+g420.start(options, function (err, proc) {
+    if (err) return console.error(err);
+    // get your g420 on!
+});
+```
+The callback's parameter `proc` is the child process, which is also attached to the `g420` object (`g420.proc`) for your convenience.
+
+When you and g420 have had enough lively times, `stop` kills the underlying g420 process:
+```javascript
+g420.stop(function () {
+    // no more lively times :( 
+});
+```
+
+### Listeners
+
+The callback for `start` fires after g420 has successfully started.  Specifically, it looks for `"IPC service started"` in g420's stderr.  If you want to change the start callback trigger to something else, this can be done by replacing g420's default listeners.  `g420.start` accepts an optional second parameter which should specify the listeners to overwrite, for example:
+```javascript
+{
+    stdout: function (data) {
+        process.stdout.write("I got a message!! " + data.toString());
+    },
+    stderr: function (data) {
+        if (data.toString().indexOf("Protocol Versions") > -1) {
+            g420.trigger(null, g420.proc);
+        }
+    },
+    close: function (code) {
+        console.log("It's game over, man!  Game over!");
+    }
+}
+```
+In the above code, `g420.trigger` is the callback passed to `g420.start`.  (This callback is stored so that the startup trigger can be changed if needed.)  These three listeners (`stdout`, `stderr`, and `close`) are the only listeners which can be specified in this parameter, since only these three are set by default in `g420.start`.
+
+If you want to swap out or add other listeners (after the initial startup), you can use the `g420.listen` convenience method:
+```javascript
+g420.listen("stdout", "data", function (data) { process.stdout.write(data); });
+```
+This example (re-)sets the "data" listener on stdout by setting `g420.proc.stdout._events.data = function (data) { process.stdout.write(data); }`.
+
+Tests
+-----
+
+g420.js tests use fourtwentyrpc to send some basic requests to g420.
+```
+$ npm test
+```
---
+++ test/test.js
@@ -0,0 +1,430 @@
+"use strict";
+
+var async = require("async");
+var join = require("path").join;
+var assert = require("chai").assert;
+var fourtwentyrpc = require("fourtwentyrpc");
+var g420 = require("../");
+g420.debug = false;
+
+var SYMLINK = join(process.env.HOME, "fourtwentylink");
+var COINBASE = {
+    "10101": "0x05ae1d0ca6206c6168b42efcd1fbe0ed144e821b",
+    "7": "0x05ae1d0ca6206c6168b42efcd1fbe0ed144e821b"
+};
+var BOOTNODES = [
+    "enode://"+
+        "d4f4e7fd3954718562544dbf322c0c84d2c87f154dd66a39ea0787a6f74930c4"+
+        "2f5d13ba2cfef481b66a6f002bc3915f94964f67251524696a448ba40d1e2b12"+
+        "@45.33.59.27:30303",
+    "enode://"+
+        "a9f34ea3de79cd75ba49c37603d28a7c494f32604b4ad6e3415b4c6020ff5bf3"+
+        "8f9772d69362c024355245fe839dd397ff9ec04db70b3258d92259323cb792ae"+
+        "@69.164.196.239:30303",
+    "enode://"+
+        "4f23a991ea8739bcc5ab52625407fcfddb03ac31a36141184cf9072ff8bf3999"+
+        "54bb94ec47e1f653a0b0fea8d88a67fa3147dbe5c56067f39e0bd5125ae0d1f1"+
+        "@139.162.5.153:30303",
+    "enode://"+
+        "bafc7bbaebf6452dcbf9522a2af30f586b38c72c84922616eacad686ab6aaed2"+
+        "b50f808b3f91dba6a546474fe96b5bff97d51c9b062b4a2e8bc9339d9bb8e186"+
+        "@106.184.4.123:30303"
+];
+
+var options = {
+    "network 10101: locked": {
+        networkid: "10101",
+        port: 30304,
+        rpcport: 8547
+    },
+    "network 10101: locked/flags": {
+        flags: {
+            networkid: "10101",
+            port: 30304,
+            rpcport: 8547
+        }
+    },
+    "network 10101: locked/symlink/flags": {
+        symlink: SYMLINK,
+        flags: {
+            networkid: "10101",
+            port: 30304,
+            rpcport: 8547
+        }
+    },
+    "network 10101: locked/persistent/flags": {
+        persist: true,
+        flags: {
+            networkid: "10101",
+            port: 30304,
+            rpcport: 8547
+        }
+    },
+    "network 10101: locked/persistent/symlink/flags": {
+        persist: true,
+        symlink: SYMLINK,
+        flags: {
+            networkid: "10101",
+            port: 30304,
+            rpcport: 8547
+        }
+    },
+    "network 7: locked": {
+        networkid: "7",
+        port: 30304,
+        rpcport: 8547
+    },
+    "network 7: locked/flags": {
+        flags: {
+            networkid: "7",
+            port: 30304,
+            rpcport: 8547
+        }
+    },
+    "network 7: locked/symlink/flags": {
+        symlink: SYMLINK,
+        flags: {
+            networkid: "7",
+            port: 30304,
+            rpcport: 8547
+        }
+    },
+    "network 7: locked/persistent/flags": {
+        persist: true,
+        flags: {
+            networkid: "7",
+            port: 30304,
+            rpcport: 8547
+        }
+    },
+    "network 7: locked/persistent/symlink/flags": {
+        persist: true,
+        symlink: SYMLINK,
+        flags: {
+            networkid: "7",
+            port: 30304,
+            rpcport: 8547
+        }
+    }
+};
+if (!process.env.CONTINUOUS_INTEGRATION) {
+    options["network 7: locked"] = {
+        networkid: "7",
+        port: 30304,
+        rpcport: 8547,
+        bootnodes: BOOTNODES
+    };
+    options["network 7: locked/flags"] = {
+        flags: {
+            networkid: "7",
+            port: 30304,
+            rpcport: 8547,
+            bootnodes: BOOTNODES
+        }
+    };
+    options["network 10101: unlocked"] = {
+        networkid: "10101",
+        port: 30304,
+        rpcport: 8547,
+        unlock: COINBASE["10101"],
+        fourtwentybase: COINBASE["10101"]
+    };
+    options["network 10101: unlocked/flags"] = {
+        flags: {
+            networkid: "10101",
+            port: 30304,
+            rpcport: 8547,
+            unlock: COINBASE["10101"],
+            fourtwentybase: COINBASE["10101"]
+        }
+    };
+    options["network 10101: account/symlink/flags"] = {
+        account: COINBASE["10101"],
+        symlink: SYMLINK,
+        flags: {
+            networkid: "10101",
+            port: 30304,
+            rpcport: 8547,
+            shh: null,
+            mine: null
+        }
+    };
+    options["network 7: unlocked"] = {
+        networkid: "7",
+        port: 30304,
+        rpcport: 8547,
+        unlock: COINBASE["7"],
+        fourtwentybase: COINBASE["7"],
+        bootnodes: BOOTNODES
+    };
+    options["network 7: unlocked/flags"] = {
+        flags: {
+            networkid: "7",
+            port: 30304,
+            rpcport: 8547,
+            unlock: COINBASE["7"],
+            fourtwentybase: COINBASE["7"],
+            bootnodes: BOOTNODES
+        }
+    };
+    options["network 7: account/symlink/flags"] = {
+        account: COINBASE["7"],
+        symlink: SYMLINK,
+        flags: {
+            networkid: "7",
+            port: 30304,
+            rpcport: 8547,
+            bootnodes: BOOTNODES
+        }
+    };
+}
+
+var TIMEOUT = 360000;
+var PROTOCOL_VERSION = "0x3f";
+
+function runtests(options) {
+
+    describe("fourtwentyrpc commands", function () {
+
+        var requests = 0;
+        var network_id = (options.flags) ? options.flags.networkid : options.networkid;
+        var coinbase = COINBASE[network_id];
+
+        before(function (done) {
+            this.timeout(TIMEOUT);
+            g420.start(options, function (err, spawned) {
+                if (err) return g420.stop(function () { done(err); });
+                if (!spawned) return g420.stop(function () { done(new Error("where's the g420?")); });
+                fourtwentyrpc.ipcpath = join(g420.datadir, "g420.ipc");
+                done();
+            });
+        });
+
+        after(function (done) {
+            this.timeout(TIMEOUT);
+            g420.stop(done);
+        });
+
+        describe("broadcast", function () {
+
+            var test = function (t) {
+                it(JSON.stringify(t.command) + " -> " + t.expected, function (done) {
+                    this.timeout(TIMEOUT);
+                    fourtwentyrpc.broadcast(t.command, function (res) {
+                        if (res.error) return done(res);
+                        assert.strictEqual(res, t.expected);
+                        done();
+                    });
+                });
+            };
+
+            test({
+                command: {
+                    id: ++requests,
+                    jsonrpc: "2.0",
+                    method: "net_listening",
+                    params: []
+                },
+                expected: true
+            });
+            test({
+                command: {
+                    id: ++requests,
+                    jsonrpc: "2.0",
+                    method: "fourtwenty_protocolVersion",
+                    params: []
+                },
+                expected: PROTOCOL_VERSION
+            });
+
+        });
+
+        describe("listening", function () {
+
+            var test = function (t) {
+                it(t.node + " -> " + t.listening, function (done) {
+                    this.timeout(TIMEOUT);
+                    fourtwentyrpc.listening(function (listening) {
+                        assert.strictEqual(listening, t.listening);
+                        done();
+                    });
+                });
+            };
+
+            test({ listening: true });
+
+        });
+
+        describe("version (network ID)", function () {
+
+            var test = function (t) {
+                it(t.node + " -> " + t.version, function (done) {
+                    this.timeout(TIMEOUT);
+                    fourtwentyrpc.version(function (version) {
+                        if (version.error) return done(version);
+                        assert.strictEqual(version, t.version);
+                        done();
+                    });
+                });
+            };
+
+            test({ version: network_id });
+
+        });
+
+        describe("Fourtwenty bindings", function () {
+
+            it("fourtwenty('protocolVersion')", function (done) {
+                fourtwentyrpc.fourtwenty("protocolVersion", null, function (res) {
+                    if (res.error) return done(res);
+                    assert.strictEqual(res, PROTOCOL_VERSION);
+                    done();
+                });
+            });
+
+            it("getGasPrice", function (done) {
+                fourtwentyrpc.getGasPrice(function (res) {
+                    if (res.error) return done(res);
+                    assert.isAbove(parseInt(res), 0);
+                    done();
+                });
+            });
+
+            if (!process.env.CONTINUOUS_INTEGRATION) {
+
+                it("blockNumber", function (done) {
+                    fourtwentyrpc.blockNumber(function (res) {
+                        if (res.error) return done(res);
+                        assert.isAtLeast(parseInt(res), 0);
+                        done();
+                    });
+                });
+
+                it("balance/getBalance", function (done) {
+                    fourtwentyrpc.balance(coinbase, function (res) {
+                        if (res.error) return done(res);
+                        assert.isAtLeast(parseInt(res), 0);
+                        fourtwentyrpc.getBalance(coinbase, function (r) {
+                            if (r.error) return done(r);
+                            assert.isAtLeast(parseInt(r), 0);
+                            assert.strictEqual(r, res);
+                            fourtwentyrpc.balance(coinbase, "latest", function (r) {
+                                if (r.error) return done(r);
+                                assert.isAtLeast(parseInt(r), 0);
+                                assert.strictEqual(r, res);
+                                fourtwentyrpc.getBalance(coinbase, "latest", function (r) {
+                                    if (r.error) return done(r);
+                                    assert.isAtLeast(parseInt(r), 0);
+                                    assert.strictEqual(r, res);
+                                    fourtwentyrpc.balance(coinbase, null, function (r) {
+                                        if (r.error) return done(r);
+                                        assert.isAtLeast(parseInt(r), 0);
+                                        assert.strictEqual(r, res);
+                                        fourtwentyrpc.getBalance(coinbase, null, function (r) {
+                                            if (r.error) return done(r);
+                                            assert.isAtLeast(parseInt(r), 0);
+                                            assert.strictEqual(r, res);
+                                            done();
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+
+                it("txCount/getTransactionCount", function (done) {
+                    fourtwentyrpc.txCount(coinbase, function (res) {
+                        if (res.error) return done(res);
+                        assert(parseInt(res) >= 0);
+                        fourtwentyrpc.pendingTxCount(coinbase, function (res) {
+                            if (res.error) return done(res);
+                            assert(parseInt(res) >= 0);
+                            done();
+                        });
+                    });
+                });
+
+            }
+
+            it("peerCount", function (done) {
+                fourtwentyrpc.peerCount(function (res) {
+                    if (res.error) return done(res);
+                    assert(parseInt(res) >= 0);
+                    done();
+                });
+            });
+
+            it("hashrate", function (done) {
+                fourtwentyrpc.hashrate(function (res) {
+                    if (res.error) return done(res);
+                    assert(parseInt(res) >= 0);
+                    done();
+                });
+            });
+
+            it("mining", function (done) {
+                fourtwentyrpc.mining(function (res) {
+                    if (res.error) return done(res);
+                    assert.isBoolean(res);
+                    done();
+                });
+            });
+
+            it("clientVersion", function (done) {
+                fourtwentyrpc.clientVersion(function (res) {
+                    if (res.error) return done(res);
+                    assert.isString(res);
+                    assert.strictEqual(res.split('/')[0], "G420");
+                    done();
+                });
+            });
+        });
+    });
+
+    describe("listeners", function () {
+
+        describe("stderr", function () {
+
+            var test = function (t) {
+                var label = (t.label) ? "'" + t.label + "'" : "";
+                it("g420.stderr(" + label + ")", function (done) {
+                    this.timeout(TIMEOUT);
+                    g420.stop(function () {
+                        g420.start(options, {
+                            stderr: function (data) {
+                                if (g420.debug) process.stdout.write(data);
+                                if (data.toString().indexOf("Starting P2P networking") > -1) {
+                                    g420.trigger(null, g420.proc);
+                                }
+                            }
+                        }, function (err, spawned) {
+                            if (err) {
+                                return g420.stop(function () { done(err); });
+                            }
+                            if (!spawned) {
+                                return g420.stop(function () { done(new Error("where's the g420?")); });
+                            }
+                            g420.stderr(t.label, function (data) {
+                                if (g420.debug) process.stdout.write(data);
+                                if (data.toString().indexOf("HTTP endpoint opened") > -1) {
+                                    g420.stop(done);
+                                }
+                            });
+                        });
+                    });
+                });
+            };
+
+            test({ label: "data" });
+            test({ label: undefined });
+        });
+    });
+}
+
+async.forEachOfSeries(options, function (opt, label, nextOpt) {
+    describe(label, function () {
+        after(nextOpt);
+        runtests(opt);
+    });
+});
